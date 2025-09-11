a221aedcac4ecc06f3214fb6d5c22c888431fe40
Vaibhav Devkate <devkatevaibhav04@gmail.com>
2025-09-11 08:51:47 +0530

    Time: 4 ms (25.24%), Space: 11.4 MB (9.02%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     string reverseVowels(string s) {
         vector<char> v;
-                vector<int> p;
+        vector<int> p;
 
-        for(int i =0 ;i<s.length();i++){
-            if(s[i]=='a' || s[i]=='e' ||s[i]=='i' ||s[i]=='o' ||s[i]=='u' ||s[i]=='A' ||s[i]=='E' ||s[i]=='I' ||s[i]=='O' ||s[i]=='U'){
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
+                s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' ||
+                s[i] == 'O' || s[i] == 'U') {
                 v.push_back(s[i]);
                 p.push_back(i);
             }
         }
-        reverse(v.begin(),v.end());
-        for(int i =0; i<p.size();i++){
-           s[p[i]]=v[i];
+        reverse(v.begin(), v.end());
+        for (int i = 0; i < p.size(); i++) {
+            s[p[i]] = v[i];
         }
 
         return s;
